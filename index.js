99a481cd2ff1c790ac737b46e06aafc83c1c0d2a
Bhupesh Bora <bhupeshbora01@gmail.com>
2024-10-20 15:57:38 +0530

    initial commit with project file

---
+++ index.js
@@ -0,0 +1,66 @@
+import express from "express"
+import axios from "axios"
+import dotenv from "dotenv"
+import bodyParser from "body-parser";
+
+// Load environment variables from .env file
+dotenv.config();
+
+const app=express();
+const port=3000; 
+
+app.set('view engine', 'ejs');
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/", async (req, res) => {
+    const city = req.query.city;
+    let weath = null;
+    let error = null;
+
+    if (city) {
+        const apiKey = process.env.API_KEY;
+        const apiUrl = `https://api.weatherbit.io/v2.0/current?key=${apiKey}&city=${city}`;
+        try {
+            const response = await axios.get(apiUrl);
+            weath = response.data;
+        } catch (error) {
+            console.error("Failed to fetch weather data", error);
+            error = "Could not fetch weather data. Please try again.";
+        }
+    }
+
+    res.render("index.ejs", {
+        weath: weath,
+        error: error,
+    });
+});
+
+app.post("/search", async (req, res) => {
+    const city = req.body.search;
+    // Redirect to the home page with the city as a query parameter
+    res.redirect(`/?city=${city}`);
+});
+
+app.get("/geo", async (req, res) => {
+    const { lat, lon } = req.query;
+    const apiKey = process.env.API_KEY;
+    const apiUrl = `https://api.weatherbit.io/v2.0/current?lat=${lat}&lon=${lon}&key=${apiKey}`;
+    
+    try {
+        const response = await axios.get(apiUrl);
+        res.json(response.data);
+    } catch (error) {
+        console.error("Failed to fetch weather data", error);
+        res.status(500).json({ error: "Could not fetch weather data. Please try again." });
+    }
+});
+
+//about
+app.get("/about",(req,res)=>{
+    res.render("about.ejs");
+ });
+    
+app.listen(port,()=>{
+    console.log("working in port 3000");
+});
