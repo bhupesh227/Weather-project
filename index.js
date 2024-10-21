00f0cbc27100c6330297c57f3eb6a4f2e15c4e1d
Bhupesh Bora <bhupeshbora01@gmail.com>
2024-10-21 12:12:42 +0530

    made some right  changes in css and made bg dynamic

--- index.js
+++ index.js
@@ -17,13 +17,41 @@ app.get("/", async (req, res) => {
     const city = req.query.city;
     let weath = null;
     let error = null;
-
+    let backgroundImage = 'default.jpg'; 
     if (city) {
         const API_KEY= process.env.API_KEY;
         const apiUrl = `https://api.weatherbit.io/v2.0/current?key=${API_KEY}&city=${city}`;
         try {
             const response = await axios.get(apiUrl);
             weath = response.data;
+             // Switch statement to set background image based on weather code
+             const weatherCode = weath.data[0].weather.code;
+
+             switch (true) {
+                 case (weatherCode >= 200 && weatherCode < 300):
+                     backgroundImage = 'twr.jpg'; // Thunderstorm
+                     break;
+                 case (weatherCode >= 300 && weatherCode < 400):
+                     backgroundImage = 'drizzle.jpg'; // Drizzle
+                     break;
+                 case (weatherCode >= 500 && weatherCode < 600):
+                     backgroundImage = 'rain.jpg'; // Rain
+                     break;
+                 case (weatherCode >= 600 && weatherCode < 700):
+                     backgroundImage = 'snow.jpg'; // Snow
+                     break;
+                 case (weatherCode >= 700 && weatherCode < 800):
+                     backgroundImage = 'mist.jpg'; // Mist
+                     break;
+                 case (weatherCode === 800):
+                     backgroundImage = 'clear_sky.jpg'; // Clear sky
+                     break;
+                 case (weatherCode > 800):
+                     backgroundImage = 'few_clouds.jpg'; // Few clouds
+                     break;
+                 default:
+                     backgroundImage = 'default.jpg'; // Fallback
+             }
         } catch (error) {
             console.error("Failed to fetch weather data", error);
             error = "Could not fetch weather data. Please try again.";
@@ -33,6 +61,7 @@ app.get("/", async (req, res) => {
     res.render("index.ejs", {
         weath: weath,
         error: error,
+        backgroundImage: backgroundImage // Pass the image name to the view
     });
 });
 
